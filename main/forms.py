d9e04da25bac7e291c2c5de770ffda1929969be2
Tarannum Khan <tkhaniitr@gmail.com>
2018-06-26 22:17:51 +0530

    Minor fixes

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,10 @@ from django import forms
 class FileUploadForm(forms.Form):
     file_desc = forms.CharField(label='File Description', max_length=100)
     file_tags = forms.CharField(label='Tags for your data',
-                                widget=forms.TextInput(attrs={'placeholder': 'comma separated values'}))
+                                widget=forms.TextInput(attrs={
+                                                       'placeholder':
+                                                       'comma separated values'
+                                                       }))
     data_file = forms.FileField(label='File to upload')
 
     def get_tags(self):
